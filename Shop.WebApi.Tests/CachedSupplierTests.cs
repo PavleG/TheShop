9291fcc57be7ca508f991f90974bfd43dd9b27f9
pavleg <pavle.gavrilovic@teletrader.com>
2022-12-19 22:27:50 +0100

    Added tests for caching supplier

---
+++ Shop.WebApi.Tests/CachedSupplierTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Shop.WebApi.Models;
+using Shop.WebApi.Services;
+
+namespace Shop.WebApi.Tests;
+
+public class CachedSupplierTests
+{
+    [Fact]
+    public void CachedSupplier_ShouldReturnArticle_WhenArticlePresentInCache()
+    {
+        IMemoryCache memoryCacheMock = new MemoryCache(new MemoryCacheOptions());
+        Mock<ISupplierService> supplierServiceMock = new Mock<ISupplierService>();
+        Article article = new Article() { Id = 23, ArticleName = "Article 23", ArticlePrice = 100};
+        memoryCacheMock.Set("article-23", article);
+
+        var sut = new CachedSupplier(memoryCacheMock, supplierServiceMock.Object);
+
+        var result = sut.GetArticleAsync(23).Result;
+        supplierServiceMock.Verify(mock => mock.GetArticleAsync(It.IsIn<int>(), It.IsAny<int>(), new CancellationToken()), Times.Never);
+        result.Should().Be(article);
+    }
+    [Fact]
+    public void CachedSupplier_ShouldCallSupplierService_WhenArticleNotInCache()
+    {
+        IMemoryCache memoryCacheMock = new MemoryCache(new MemoryCacheOptions());
+        Mock<ISupplierService> supplierServiceMock = new Mock<ISupplierService>();
+
+        var sut = new CachedSupplier(memoryCacheMock, supplierServiceMock.Object);
+
+        var id = It.IsAny<int>();
+        var result = sut.GetArticleAsync(id).Result;
+        result.Should().BeNull();
+        supplierServiceMock.Verify(mock => mock.GetArticleAsync(id, It.IsAny<int>(), new CancellationToken()), Times.Once);
+    }
+}
