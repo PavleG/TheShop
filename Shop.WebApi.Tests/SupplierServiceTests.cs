a389ae439dcf85dd1b542c7859a604732c5cb1e5
pavleg <pavle.gavrilovic@teletrader.com>
2022-12-20 00:29:56 +0100

    Attemted to write supplier tests

---
+++ Shop.WebApi.Tests/SupplierServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using Shop.WebApi.Models;
+using Shop.WebApi.Repositories;
+using Shop.WebApi.Services;
+
+namespace Shop.WebApi.Tests;
+
+public class SupplierServiceTests
+{
+    // SupplierService depends on the concrete implementations
+    // of IArticleProvider and it's not straightforward to mock
+    // their behavior...
+
+    //[Fact]
+    //public void SomeTest()
+    //{
+    //    Mock<IArticleProvider> mockWarehouse = new Mock<IArticleProvider>();
+    //    Mock<IArticleProvider> mockVendor1 = new Mock<IArticleProvider>();
+    //    Mock<IArticleProvider> mockVendor2 = new Mock<IArticleProvider>();
+    //    var article = new Article()
+    //    {
+    //        Id = 10,
+    //        ArticleName = "Article 10",
+    //        ArticlePrice = 100
+    //    };
+    //    mockWarehouse.Setup(mock => mock.ArticleInInventoryAsync(10, new CancellationToken()).Result).Returns(true);
+    //    mockWarehouse.Setup(mock => mock.GetArticleAsync(10, new CancellationToken()).Result).Returns(article);
+    //    IEnumerable<IArticleProvider> articleProviders = new List<IArticleProvider>()
+    //    {
+    //        mockWarehouse.Object,
+    //        mockVendor1.Object,
+    //        mockVendor2.Object,
+    //    };
+
+    //    var sut = new SupplierService(articleProviders);
+
+    //    var result = sut.GetArticleAsync(10, int.MaxValue).Result;
+
+    //    result.Should().Be(article);
+    //}
+}
